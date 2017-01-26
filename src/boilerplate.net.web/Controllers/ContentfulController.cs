32d6bb40a1e8503786ad0b6e0b4fbd987345e701
Robert Linde <robert@selberglinde.se>
2017-01-26 14:26:58 +0100

    First commit

---
+++ src/boilerplate.net.web/Controllers/ContentfulController.cs
@@ -0,0 +1,38 @@
+﻿using boilerplate.net.web.Models;
+using Contentful.Core;
+using Contentful.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace boilerplate.net.web.Controllers
+{
+    public class ContentfulController : Controller
+    {
+        private readonly IContentfulClient _client;
+
+        public ContentfulController(IContentfulClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var space = await _client.GetSpaceAsync();
+            var entries = await _client.GetEntriesAsync<Entry<dynamic>>();
+            var assets = await _client.GetAssetsAsync();
+
+            var contentfulExampleModel = new ContentfulExampleModel()
+            {
+                Space = space,
+                Entries = entries.ToList(),
+                Assets = assets.ToList()
+            };
+
+            return View(contentfulExampleModel);
+        }
+    }
+}
---
+++ src/boilerplate.net.web/Models/ContentfulExampleModel.cs
@@ -0,0 +1,17 @@
+﻿using Contentful.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace boilerplate.net.web.Models
+{
+    public class ContentfulExampleModel
+    {
+        public Space Space { get; set; }
+
+        public List<Entry<dynamic>> Entries { get; set; }
+
+        public List<Asset> Assets { get; set; }
+    }
+}
