9d11a011cebf5e7ae435222fe8cd65014575993f
Robert Linde <robert@selberglinde.se>
2017-02-03 09:17:14 +0100

    Add console project

---
+++ src/Boilerplate.Net.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using Contentful.Core;
+using Contentful.Core.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var httpClient = new HttpClient();
+        var client = new ContentfulClient(
+            httpClient,
+            "<delivery_api_key>",
+            "<space_id>");
+        var entries = client.GetEntriesAsync<Entry<dynamic>>().Result;
+        Console.WriteLine(JsonConvert.SerializeObject(entries, Formatting.Indented));
+        Console.ReadLine();
+    }
+}
